1fb53e34ae55ce267e76c766c1df85890b801010
Khrystyna T <tyvoniuk.khrystyna@gmail.com>
2023-08-19 10:56:28 +0200

    added info on compaund assignments

---
+++ BASICS/practice/compaund-assignments.js
@@ -0,0 +1,19 @@
+// Addition and Assignment (+=)
+let num = 10;
+num += 5; // Equivalent to: num = num + 5;
+document.write("After addition: num = " + num + "<br>");
+
+// Subtraction and Assignment (-=)
+let total = 100;
+total -= 20; // Equivalent to: total = total - 20;
+document.write("After subtraction: total = " + total + "<br>");
+
+// Multiplication and Assignment (*=)
+let price = 50;
+price *= 2; // Equivalent to: price = price * 2;
+document.write("After multiplication: price = " + price + "<br>");
+
+// Division and Assignment (/=)
+let quantity = 8;
+quantity /= 2; // Equivalent to: quantity = quantity / 2;
+document.write("After division: quantity = " + quantity + "<br>");
---
+++ BASICS/practice/variables.js
@@ -0,0 +1,17 @@
+// Declaring a Variable
+var age;
+let name;
+const pi = 3.14;
+
+//In this step, you're essentially reserving a spot in memory for the variable but not yet giving it a value.
+
+// Assigning a Variable:
+age = 25;
+name = "John";
+
+//At this point, you're giving the variable a value that it will hold in memory.
+
+//You can also combine declaration and assignment into a single step:
+
+var age = 25; // Declaring and assigning 'age' in one step
+let name1 = "John"; // Declaring and assigning 'name' in one step
