45f60e3bb01c3680dff97c9c9ccce66a45e55c51
KhrystynaT <tyvoniuk.khrystyna@gmil.com>
2023-08-20 20:12:23 +0200

    created increment function

--- Counter/index.js
+++ Counter/index.js
@@ -1,1 +1,8 @@
-document.getElementById("count-el").innerText = 5;
+// change the count-el in the html to reflect the new count
+let count = 0;
+function increment() {
+  count = count + 1;
+  document.getElementById("count-el").innerText = count;
+
+  console.log(count);
+}
