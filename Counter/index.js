bb890ef6705794bba17196f4f77ca23af18a3352
KhrystynaT <tyvoniuk.khrystyna@gmil.com>
2023-08-21 09:43:09 +0200

    created save-btn to save count

--- Counter/index.js
+++ Counter/index.js
@@ -1,8 +1,18 @@
 // change the count-el in the html to reflect the new count
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 let count = 0;
 function increment() {
-  count = count + 1;
-  document.getElementById("count-el").innerText = count;
+  count += 1;
+  countEl.innerText = count;
+
+  console.log(count);
+}
+
+// create a function, save(), which logs out the count when its called
+function save() {
+  let entry = count + "-";
+  saveEl.innerText += entry;
 
   console.log(count);
 }
