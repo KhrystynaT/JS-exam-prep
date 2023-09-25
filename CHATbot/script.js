7a1dc1b1194e42f1629dacfb9d1fb998c6a77c6b
KhrystynaT <tyvoniuk.khrystyna@gmail.com>
2023-09-25 23:56:11 +0200

    added islamic phrases

--- CHATbot/script.js
+++ CHATbot/script.js
@@ -7,30 +7,57 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 const utterances = [
-  ["how are you", "how is life", "how are things"], //0
-  ["hi", "hey", "hello", "good morning", "good afternoon"], //1
-  ["what are you doing", "what is going on", "what is up"], //2
-  ["how old are you"], //3
-  ["who are you", "are you human", "are you bot", "are you human or bot"], //4
+  ["how are you", "how is life", "how are things"], // 0
+  [
+    "hi",
+    "salam",
+    "merhaba",
+    "salam aleykum",
+    "hey",
+    "hello",
+    "good morning",
+    "good afternoon",
+  ], // 1
+  ["what are you doing", "what is going on", "what is up"], // 2
+  ["how old are you"], // 3
+  ["who are you", "are you human", "are you bot", "are you human or bot"], // 4
+  ["assalamu alaikum", "wa alaikum assalam"], // 5 - Common Islamic greeting
+  ["ramadan kareem", "eid mubarak", "hajj mabrur"], // 6 - Islamic holiday greetings
+  ["quran", "hadith", "sunnah"], // 7 - Islamic religious terms
+  ["masjid", "salaat", "zakat", "hijab"], // 8 - Islamic practices and concepts
 ];
 
 // Possible responses corresponding to triggers
 
 const answers = [
   [
-    "Fine... how are you?",
+    "Fine...Alhamdulillah. How are you?",
+    "Alhamdulillah, and you?",
     "Pretty well, how are you?",
     "Fantastic, how are you?",
-  ], //0
-  ["Hello!", "Hi!", "Hey!", "Hi there!", "Howdy"], //1
+  ], // 0
+  ["Salam!", "Hi!", "Hey!", "Hi there!", "Howdy"], // 1
   [
     "Nothing much",
     "About to go to sleep",
     "Can you guess?",
     "I don't know actually",
-  ], //2
-  ["I am infinite"], //3
-  ["I am just a bot", "I am a bot. What are you?"], //4
+  ], // 2
+  ["I am infinite"], // 3
+  ["I am just a bot", "I am a bot. What are you?"], // 4
+  ["Wa alaikum assalam!", "Wa alaikum assalam! How can I assist you today?"], // 5 - Response to Islamic greetings
+  ["Ramadan Kareem!", "Eid Mubarak!", "May your Hajj be accepted!"], // 6 - Response to Islamic holiday greetings
+  [
+    "The Quran is the holy book of Islam.",
+    "Hadith are the sayings and actions of Prophet Muhammad (peace be upon him).",
+    "Sunnah refers to the traditions and practices of the Prophet (peace be upon him).",
+  ], // 7 - Response to Islamic religious terms
+  [
+    "A masjid is a place of worship for Muslims.",
+    "Salat is the Islamic practice of prayer.",
+    "Zakat is a form of almsgiving in Islam.",
+    "Hijab is a modest dress code for Muslim women.",
+  ], // 8 - Response to Islamic practices and concepts
 ];
 
 // For any other user input
